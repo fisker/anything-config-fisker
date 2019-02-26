4e9807a3916e7b49a39751f08975d51b4f69e397
fisker <lionkay@gmail.com>
2019-02-26 21:38:51 +0800

    fix: safer dependencies check

--- src/utils/is-dependency-added.js
+++ src/utils/is-dependency-added.js
@@ -1,11 +1,12 @@
 import pkg from './pkg'
 import hasOwn from './has-own'
 
+const dependencies = {
+  ...pkg.dependencies,
+  ...pkg.devDependencies,
+}
 function isDependencyAdded(dependency) {
-  return (
-    hasOwn.call(pkg.dependencies, dependency) ||
-    hasOwn.call(pkg.devDependencies, dependency)
-  )
+  return hasOwn.call(dependencies, dependency)
 }
 
 export default isDependencyAdded
