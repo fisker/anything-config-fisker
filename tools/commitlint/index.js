99b6dd62cfdd82b6a4074ea1218dd6899b1f8a7b
fisker <lionkay@gmail.com>
2019-04-18 20:41:04 +0800

    feat: use `@fisker/commitlint-config`

--- tools/commitlint/index.js
+++ tools/commitlint/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   effects: {
     files: ['commitlint.config.js'],
-    dependencies: ['@commitlint/cli', '@commitlint/config-conventional'],
+    dependencies: ['@commitlint/cli', '@fisker/commitlint-config'],
     'package.json': [],
   },
 }
