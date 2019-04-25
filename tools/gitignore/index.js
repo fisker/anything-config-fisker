c10901e925f6d4092864c26dd22147e7685c3119
fisker <lionkay@gmail.com>
2019-04-25 14:51:57 +0800

    chore: ignore

---
+++ tools/gitignore/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'gitignore',
+  effects: {
+    files: [
+      {
+        source: 'gitignore',
+        dest: '.gitignore',
+      },
+    ],
+    dependencies: [],
+    'package.json': {},
+  },
+}
