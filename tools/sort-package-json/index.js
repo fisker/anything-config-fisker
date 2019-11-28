2f0e638dd7b26924ca47ed0f4755ad14e6df1b1b
fisker <lionkay@gmail.com>
2019-11-28 20:11:29 +0800

    feat: add `sort-package-json`

---
+++ tools/sort-package-json/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  effects: {
+    files: [],
+    dependencies: ['sort-package-json', 'npm-run-all'],
+    packageJson: {
+      'scripts["format:sort-package-json"]':
+        'sort-package-json **/package.json',
+    },
+  },
+}
