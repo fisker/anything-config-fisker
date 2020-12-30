805bfb4973aeaf1bdb0ba39ffdd49aa97c16dd8b
fisker <lionkay@gmail.com>
2020-12-30 15:53:33 +0800

    chore: update dependencies

--- src/tools/has-effects.js
+++ src/tools/has-effects.js
@@ -1,5 +1,7 @@
 function hasEffects({files, dependencies, pkg: package_}) {
-  return files.length > 0 || dependencies.length > 0 || package_.length > 0
+  return (
+    files.length !== 0 || dependencies.length !== 0 || package_.length !== 0
+  )
 }
 
 export default hasEffects
