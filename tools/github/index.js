92c93f4f468bbb57643f9707813af0b0d93eaa1e
fisker <lionkay@gmail.com>
2025-03-09 21:32:31 +0800

    Support automated-fix.yml

--- tools/github/index.js
+++ tools/github/index.js
@@ -1,10 +1,24 @@
+import fs from 'node:fs/promises'
+
 export default {
   name: 'GitHub',
-  *process({copyFile, removeFile}) {
+  async *process({copyFile, removeFile, writeFile, packageJson}) {
     yield removeFile('.github/funding.yml')
     yield copyFile(
-      new URL('./files/continuous-integration.yml', import.meta.url),
+      new URL(
+        './files/.github/workflows/continuous-integration.yml',
+        import.meta.url,
+      ),
       '.github/workflows/continuous-integration.yml',
+    )
+
+    const content = await fs.readFile(
+      new URL('./files/.github/workflows/automated-fix.yml', import.meta.url),
+      'utf8',
+    )
+    yield writeFile(
+      '.github/workflows/automated-fix.yml',
+      content.replaceAll('<REPOSITORY>', `fisker/${packageJson.name}`),
     )
   },
 }
